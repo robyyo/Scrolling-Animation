6e303d9dbfb491ada2955c5019115f9788c0b7d0
Robert Finkley <me@robertfinkley.com>
2021-11-12 13:30:04 -0500

    finished project

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const boxes = document.querySelectorAll('.box');
 window.addEventListener('scroll', checkBoxes);
 checkBoxes();
 function checkBoxes() {
-  console.log(window.innerHeight);
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
